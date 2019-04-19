43071f87f62c08f055e9e1912328525c815f0841
Rizvane <rizvane95140@gmail.com>
2019-04-19 10:09:55 +0200

    Ajout de l'API pour récup. les films/Ajout de la vue

---
+++ API/TMDBApi.js
@@ -0,0 +1,11 @@
+// API/TMDBApi.js
+
+const API_TOKEN = "d76ebf95005f232efab4e0de0288a840";
+
+export function getFilmsFromApiWithSearchedText (text) {
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=fr&query=' + text
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.log(error))
+
+}
--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -1,38 +1,30 @@
 // Components/FilmItem.js
 
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Image } from 'react-native'
 
 class FilmItem extends React.Component {
     render() {
+        const film = this.props.film
+
         return (
             <View style={styles.main_container}>
-                <View style={styles.image} >
-                    <Text>Image du film</Text>
-                </View>
-                <View style={{flex:2, flexDirection: 'column', backgroundColor: 'green'}} >
-
-                    <View style={{flex:1,flexDirection: 'row', backgroundColor: 'orange'}}>
-
-                        <View style={styles.titre_film}>
-                            <Text>Titre du film (Text)</Text>
-
-                        </View>
-                        <View style={styles.vote}>
-                            <Text>Vote (Text)</Text>
-
-                        </View>
+                <Image
+                    style={styles.image}
+                    source={{uri: "image"}}
+                />
+                <View style={styles.content_container}>
+                    <View style={styles.header_container}>
+                        <Text style={styles.title_text}>{film.title}</Text>
+                        <Text style={styles.vote_text}>{film.vote_average}</Text>
                     </View>
-
-                    <View style={styles.description} >
-                        <Text>Description (Text)</Text>
+                    <View style={styles.description_container}>
+                        <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
+                        {/* La propriété numberOfLines permet de couper un texte si celui-ci est trop long, il suffit de définir un nombre maximum de ligne */}
                     </View>
-
-                    <View style={styles.date} >
-                        <Text>Sortie le 00/00/0000 (Text)</Text>
+                    <View style={styles.date_container}>
+                        <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
                     </View>
-
-
                 </View>
             </View>
         )
@@ -42,36 +34,47 @@ class FilmItem extends React.Component {
 const styles = StyleSheet.create({
     main_container: {
         height: 190,
-        backgroundColor: 'red',
         flexDirection: 'row'
     },
     image: {
-        flex:1,
-        backgroundColor: 'blue'
+        width: 120,
+        height: 180,
+        margin: 5,
+        backgroundColor: 'gray'
     },
-    titre_film: {
-        flex:3,
-        backgroundColor: 'orange',
-        borderColor: '#000000',
-        borderWidth: 1,
-    },
-    vote: {
-        flex:1,
-        backgroundColor: 'orange',
-        borderColor: '#000000',
-        borderWidth: 1
-    },
-    description: {
-        flex: 2,
-        backgroundColor: 'red',
-        borderColor: '#000000',
-        borderWidth: 1,
-    },
-    date: {
+    content_container: {
         flex: 1,
-        backgroundColor: 'violet',
-        borderColor: '#000000',
-        borderWidth: 1,
+        margin: 5
+    },
+    header_container: {
+        flex: 3,
+        flexDirection: 'row'
+    },
+    title_text: {
+        fontWeight: 'bold',
+        fontSize: 20,
+        flex: 1,
+        flexWrap: 'wrap',
+        paddingRight: 5
+    },
+    vote_text: {
+        fontWeight: 'bold',
+        fontSize: 26,
+        color: '#666666'
+    },
+    description_container: {
+        flex: 7
+    },
+    description_text: {
+        fontStyle: 'italic',
+        color: '#666666'
+    },
+    date_container: {
+        flex: 1
+    },
+    date_text: {
+        textAlign: 'right',
+        fontSize: 14
     }
 })
 
